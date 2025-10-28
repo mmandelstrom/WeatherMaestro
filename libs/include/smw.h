51abe0ca0f69b1ffbe23e59f7c34d8e726024192
Alexander Safapay <alexander.safapay@gmail.com>
2025-10-28 17:40:37 +0100

    added smw

--- libs/include/smw.h
+++ libs/include/smw.h
@@ -8,3 +8,34 @@
     #define smw_max_tasks 16
 #endif
 
+typedef struct
+{
+    void* context;
+     /*replace callback with work function from other module?*/
+    void (*callback)(void* _Context, uint64_t _MonTime)
+
+} smw_task;
+
+
+typedef struct
+{
+    smw_task tasks[smw_max_tasks];
+
+} smw;
+
+extern smw g_smw;
+
+int smw_init();
+
+smw_task* smw_create_task(void* _Context, void (*_Callback)(void* _Context, uint64_t _MonTime));
+void smw_destroyTask(smw_task* _Task);
+
+void smw_work(uint64_t _MonTime);
+
+int smw_getTaskCount();
+
+void smw_dispose();
+
+#endif
+
+
--- utils/src/utils.c
+++ utils/src/utils.c
@@ -1,3 +1,5 @@
+#define _POSIX_C_SOURCE 199309L
+
 #include "../include/utils.h"
 #include <time.h>
 #include <stdint.h>
