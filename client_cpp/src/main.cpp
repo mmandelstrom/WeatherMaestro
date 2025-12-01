b50165316f3f91af9445625223fce72e4d1eb7a3
CitizenSmithDev <citizensmith.business@gmail.com>
2025-12-01 14:34:47 +0100

    Added wrapper functions for socket communication

--- client_cpp/src/main.cpp
+++ client_cpp/src/main.cpp
@@ -4,9 +4,19 @@
 int main() {
   TCP_Client cli("stockholm2.onvo.se", "81");
 
-  if (cli.is_ready()) {
-    std::cout << "Ready!" << std::endl;
-  }
+  std::string request =
+    "GET /api/v1/weather?latitude=52.125&longitude=53.123445463624674272457 HTTP/1.1\r\n"
+    "Host: stockholm2.onvo.se:81\r\n"
+    "User-Agent: MyClient/1.0\r\n"
+    "Accept: /\r\n"
+    "Connection: close\r\n"
+    "\r\n";
+
+  cli.setTransmitData(request);
+  cli.transmit();
+
+  cli.recieve();
+
 
   return 0; 
 }
