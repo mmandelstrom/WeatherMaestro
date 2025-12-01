5c6f574d9599885143f50cca5c84c9e50f90e3a8
CitizenSmithDev <citizensmith.business@gmail.com>
2025-12-01 14:34:47 +0100

    Added wrapper functions for socket communication

--- client_cpp/src/tcp_client.cpp
+++ client_cpp/src/tcp_client.cpp
@@ -1,7 +1,7 @@
 #include "tcp_client.hpp"
 
 TCP_Client::TCP_Client(std::string _Host, std::string _Port) 
-    : fd(-1), ready(false)
+    : fd(-1), ready(false), transmit_length(0)
 {
     std::cout << "TCP CLI CTOR" << std::endl; //DEBUG
 
@@ -24,6 +24,8 @@ TCP_Client::TCP_Client(std::string _Host, std::string _Port)
         fd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
         if (fd < 0) {continue;}
 
+        this->set_nonblocking(fd);
+
         if (int conn = connect(fd, res->ai_addr, res->ai_addrlen) == 0) {
             break;
         } else if (conn == -1 && errno == EINPROGRESS) {
@@ -38,6 +40,7 @@ TCP_Client::TCP_Client(std::string _Host, std::string _Port)
     freeaddrinfo(res);
     
     if (fd >= 3) {
+    this->fd = fd;
     std::cout << "Socket successfully connected at fd " << fd << "!" << std::endl;
     ready = true;
     }
@@ -49,21 +52,83 @@ int TCP_Client::set_nonblocking(int _Fd){
     return 0;
 }
 
-int TCP_Client::read() {
+int TCP_Client::recieve() {
     if (this->fd < 0) {
         return -1;
     }
-    //recv(this->fd, , , MSG_DONTWAIT); //TODO: Add reading capability.
-    return 0; //temp
+
+    bool done = false;
+    char recv_buf[1024*1024];
+    char* buf_ptr = recv_buf;
+    int buf_len = 0;
+    int bytes_recieved = 0;
+    
+    while (!done) {
+    
+        bytes_recieved = recv(this->fd, buf_ptr, sizeof(recv_buf), MSG_DONTWAIT);
+
+        if (bytes_recieved < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN || errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+
+        if (bytes_recieved == 0) {
+            done = true;
+        }
+
+        if (bytes_recieved > 0) {
+            buf_ptr += bytes_recieved;
+            buf_len += bytes_recieved;
+        }
+    }
+
+    recv_buf[buf_len] = '\0';
+
+    std::string s(recv_buf, sizeof(buf_len));
+    this->receive_data = s;
+
+    std::cout << "Recieve data: " << this->receive_data << std::endl; //DEBUG
+    
+    return 0; 
 }
 
-int TCP_Client::write() {
-    return send(this->fd, this->transmit_data.c_str(), this->transmit_data.size(), MSG_NOSIGNAL);
+int TCP_Client::transmit() {
+    if (this->fd < 0 || this->transmit_length <= 0 || this->transmit_data.empty()) {
+        return -1;
+    }
+
+    int transmit_bytes = this->transmit_length;
+    int bytes_transmitted = 0;
+
+    while (transmit_bytes > 0)
+    {
+        bytes_transmitted = send(this->fd, this->transmit_data.c_str(), this->transmit_data.size(), MSG_NOSIGNAL);
+        std::cout << "Bytes_transmitted: " << bytes_transmitted << std::endl;
+        perror("send");
+
+        if (bytes_transmitted < 0) {
+            if (errno == EWOULDBLOCK || errno == EAGAIN || errno == EINTR) {
+                continue;
+            }
+            return -1;
+        }
+
+        if (bytes_transmitted > 0) {
+            transmit_bytes -= bytes_transmitted;
+        }
+    }
+    
+    return 0;
 }
 
-int TCP_Client::getFileDescriptor() {
-    return this->fd;
+void TCP_Client::setTransmitData(std::string _Data) {
+    this->transmit_data = _Data;
+    this->transmit_length = _Data.length();
 }
+
+int TCP_Client::getFileDescriptor() {return this->fd;}
 
 bool TCP_Client::is_ready() {return ready;}
 
